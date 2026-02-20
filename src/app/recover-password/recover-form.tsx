1c1f8b1769654de7852c54eace0c54e957d64d23
Jackson Costa <jacksoncostaleites2017@gmail.com>
2026-02-20 17:41:17 -0300

    Melhorado tela de recuperação de senha

--- src/app/recover-password/recover-form.tsx
+++ src/app/recover-password/recover-form.tsx
@@ -1,17 +1,37 @@
+"use client";
+
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
   CardContent,
+  CardFooter,
   CardAction,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "@/firebase/firebase-config";
 
 export default function RecoverForm() {
+  const handleResetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Email de recuperação enviado!");
+    } catch (error) {
+      console.error("Erro ao enviar email de recuperação:", error);
+      alert("Erro ao enviar email de recuperação. Tente novamente.");
+    }
+  };
+
   return (
-    <Card className="">
+    <Card className="w-full max-w-md">
       <CardHeader>
         <CardTitle>Recuperar senha</CardTitle>
         <CardDescription>
@@ -20,11 +40,15 @@ export default function RecoverForm() {
       </CardHeader>
 
       <CardContent>
-        <Input placeholder="Email" />
+        <form action="post">
+          <Input placeholder="Email" name="email" />
+        </form>
       </CardContent>
-      <CardAction>
-        <Button>Recuperar senha</Button>
-      </CardAction>
+      <CardFooter>
+        <CardAction>
+          <Button type="submit">Recuperar senha</Button>
+        </CardAction>
+      </CardFooter>
     </Card>
   );
 }
